20a79d7cce2bb6bc8376c4210bcd158b973fcb77
closzach <constantinloszach@gmail.com>
2025-11-30 15:31:18 +0100

    Amélioration Update User.

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from .models import *
-from django.contrib.auth.models import Group
 
 class AuteurSerializer(ModelSerializer):
     class Meta:
@@ -22,6 +21,16 @@ class UserSerializer(ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        
+        if password:
+            user.set_password(password)
+            user.save()
+            
         return user
 
 class LivreSerializer(ModelSerializer):
