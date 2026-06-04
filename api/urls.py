0b6b2af0212356ce1595610e1b2cdd7cd1b4baea
closzach <constantinloszach@gmail.com>
2026-06-04 22:11:23 +0200

    Correction url swagger.

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,23 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 router = DefaultRouter()
 router.register('auteurs', AuteurViewSet)
@@ -15,6 +32,7 @@ router.register('lectures', LectureViewSet, basename='lecture')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
